605275086aa9375ea28ba2c2bff0d26b8ff0a8cd
Ramzat Ruslanov <ruslanov595@gmail.com>
2025-05-24 22:30:46 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+// import React from "react";
+import "./scss/app.scss";
+import { Header } from "./Components/Header";
+import { Categories } from "./Components/Categories";
+import { Sort } from "./Components/Sort";
+import { PizzaBlock } from "./Components/PizzaBlock";
+
+export const App = () => {
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            <PizzaBlock title="Мексиканская" price="1900" />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
